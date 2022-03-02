ec7e9ae165fb73c3bdb6980c25a40d8d16b95917
venkatesh.k <brendomvenki268@gmail.com>
2022-03-02 22:52:28 +0530

    new code

--- src/org/git/GitSample.java
+++ src/org/git/GitSample.java
@@ -8,9 +8,14 @@ public class GitSample {
 	private void tc2() {
 		System.out.println("test 3");
 	}
-	
+
 	private void tc5() {
 		System.out.println("array");
+
 	}
-	
+
+	private void tc6() {
+		System.out.println("venom");
+	}
+
 }
